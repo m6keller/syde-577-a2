ea1d6345c701645e7288b001aa1b9f94ddff99c8
Adele Younis <adeleyounis@eduroam-campus-10-36-203-29.campus-dynamic.uwaterloo.ca>
2025-10-31 12:13:08 -0400

    Updated BasicCNN to account for dropout

--- BasicCNN.py
+++ BasicCNN.py
@@ -6,7 +6,19 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 class BasicCNN(nn.Module):
-    def __init__(self, num_conv_layers=3, initial_channels=16, channel_multiplier=2, num_classes=10):
+    def __init__(self, num_conv_layers=3, initial_channels=16, channel_multiplier=2, 
+                 num_classes=10, dropout_fc=0.0, dropout_conv=0.0):
+        
+        """
+        A basic CNN architecture for MNIST classification.
+        Args:
+            num_conv_layers (int): Number of convolutional layers.
+            initial_channels (int): Number of channels in the first convolutional layer.
+            channel_multiplier (float): Factor to multiply channels after each conv layer.
+            num_classes (int): Number of output classes.
+            dropout_fc (float): Dropout rate for fully connected layers.
+            dropout_conv (float): Dropout rate for convolutional layers.
+        """
         super(BasicCNN, self).__init__()
         
         layers = []
@@ -26,14 +38,24 @@ class BasicCNN(nn.Module):
             )
             layers.append(nn.ReLU(inplace=True))
             layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
-            
+
+            # Optional dropout after each conv block
+            if dropout_conv > 0:
+                layers.append(nn.Dropout2d(p=dropout_conv))
+
             in_channels = current_channels
             current_channels = int(current_channels * channel_multiplier)
             
         self.features = nn.Sequential(*layers)
         
         self.flatten = nn.Flatten()
-        self.classifier = nn.LazyLinear(num_classes)
+        if dropout_fc > 0:
+            self.classifier = nn.Sequential(
+                nn.Dropout(p=dropout_fc),
+                nn.LazyLinear(num_classes)
+            )
+        else:
+            self.classifier = nn.LazyLinear(num_classes)
 
 
     def forward(self, x):
